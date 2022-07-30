0853ec21cff1337f07acaf48b3ce1c8777957c89
brian-root <1092249319@qq.com>
2022-07-30 21:19:35 +0800

    js basic 03

--- js_basic/basic.js
+++ js_basic/basic.js
@@ -1,9 +1,5 @@
 const kawa = 'history is history';
 
-export const objs = {
-    name:'Brian'
-};
-
 const fns = () => {
     console.log('fns()');    
 }
@@ -11,4 +7,9 @@ const fns = () => {
 //1.默认导出  （一个模块中只能有一个默认导出）
 export default kawa;
 
-//2.命名导出sss
+//2.命名导出
+export const objs = {
+    name:'Brian'
+};
+
+export {fns};
