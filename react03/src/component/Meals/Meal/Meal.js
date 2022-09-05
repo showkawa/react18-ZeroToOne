b5823a8d53b95b626fd5e6a8c1998b7df77fd6ce
brian-root <1092249319@qq.com>
2022-09-05 22:13:35 +0800

    import icon from fontawesome

--- react03/src/component/Meals/Meal/Meal.js
+++ react03/src/component/Meals/Meal/Meal.js
@@ -12,7 +12,7 @@ const Meal = () => {
                 <p className={classes.Desc}>新奥尔良烤翅翅根和2块新奥尔良烤翅翅中。热辣香骨鸡桶含16块热辣香骨鸡。大堡口福桶含1个新奥尔良烤鸡腿堡，2块新奥尔良烤翅，2块香辣鸡翅，1块吮指原味鸡，1个红豆派，1份醇香土豆泥，2杯百事可乐（中）</p>
                 <div className={classes.PriceWapper}>
                     <span className={classes.Price}>12</span>
-                    <Counter amount={0}/>
+                    <Counter amount={1}/>
                 </div>
             </div>
         </div>
--- react03/src/component/UI/Counter/Counter.js
+++ react03/src/component/UI/Counter/Counter.js
@@ -1,4 +1,6 @@
 import classes from './Counter.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const Counter = (props) => {
     return (
@@ -6,12 +8,16 @@ const Counter = (props) => {
             {
                 (props.amount && props.amount !== 0) ? (
                     <>
-                        <button className={classes.Sub}>-</button>
+                        <button className={classes.Sub}>
+                            <FontAwesomeIcon icon={faMinus} />
+                        </button>
                         <span className={classes.Count}>{props.amount}</span>
                     </>
                 ) : null
             }
-            <button className={classes.Add}>+</button>
+            <button className={classes.Add}>
+                <FontAwesomeIcon icon={faPlus} />
+            </button>
         </div>);
 }
 
